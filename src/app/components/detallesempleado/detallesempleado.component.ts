8e65f77fdddb2e9d28806846d49cc75a29c60630
Carlos00-pixel <carlos.mayoral00@gmail.com>
2022-11-17 14:20:56 +0100

    first commit

---
+++ src/app/components/detallesempleado/detallesempleado.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from 'src/app/models/empleado';
+import { EmpleadosService } from 'src/app/services/empleados.service';
+import { ActivatedRoute, Params } from '@angular/router'; 
+
+@Component({
+  selector: 'app-detallesempleado',
+  templateUrl: './detallesempleado.component.html',
+  styleUrls: ['./detallesempleado.component.css']
+})
+export class DetallesempleadoComponent implements OnInit {
+
+  public empleado !: Empleado; 
+
+  public id !: number;
+
+  constructor(
+    private _service: EmpleadosService,
+    private _activeRoute: ActivatedRoute
+    ) { }
+
+  ngOnInit(): void {
+
+    this._activeRoute.params.subscribe(( parametros: Params ) => { 
+    
+      if (parametros['id'] != null){
+      
+        this.id = parseInt(parametros['id']);
+
+        this._service.getEmpleadoPorID(this.id).subscribe(response => {
+          this.empleado = response;
+        });
+      
+      } 
+      
+    }); 
+  }
+}
